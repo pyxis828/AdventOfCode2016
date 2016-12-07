426cdb12bd8fce54562a7fd0c537ace27e525682
pyxis828 <kirstinwalsh0805@gmail.com>
2016-12-06 22:08:19 -0500

    Day6 parts1&2

---
+++ Documents/Visual Studio 2015/AdventOfCode2016/Message/Message/Message.py
@@ -0,0 +1,23 @@
+﻿import os
+from collections import Counter
+from operator import itemgetter
+
+fn = os.path.join(os.path.dirname(__file__), 'message.txt')
+with open(fn) as f:
+    lines = f.readlines()
+for line in lines:
+    length = len(line)
+    columns = [ [] for _ in range(length)]
+for line in lines:
+    line = line.strip()
+    for i in range (length):
+        columns[i].append(line[i])
+word = ''
+for column in columns:
+    num_letters = Counter(column)
+    sortLetters = sorted(num_letters.items(), key=itemgetter(1))
+    index = len(sortLetters)
+    #letter = sortLetters[index-1][0]  # part 1
+    letter = sortLetters[0][0]         # part 2
+    word = word + letter
+print(word)
